52236630134ef0140b36fb07b610cf7cbf2d5748
user <user@DESKTOP-7G66QFG>
2019-12-29 12:58:03 +0900

    4.2.1.2 GET방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,10 +8,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 import java.io.File;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,37 +27,50 @@ public class RequestHandler extends Thread {
 	}
 
 	public void run() {
-		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-				connection.getPort());
+		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = br.readLine();
 			log.debug("request line : {}", line);
-			
+
 			if (line == null) {
 				return;
 			}
-			
-			String[] tokens =line.split(" ");
+
+			String[] tokens = line.split(" ");
 			log.debug("tokens : {}", tokens);
 
 			while (!line.equals("")) {
 				line = br.readLine();
-				log.debug("header : {}", line);
+//				log.debug("header : {}", line);
 			}
 
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-			DataOutputStream dos = new DataOutputStream(out);
-//			byte[] body = "Hello World".getBytes();
-			byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
-			response200Header(dos, body.length);
-			responseBody(dos, body);
+			String url = tokens[1];
+			log.debug("url : {}", url);
+//			log.debug("url : {}, File.separator : {} ", url, File.separator);
+
+			if (url.startsWith("/user/create")) {
+				int index = url.indexOf("?");
+				String queryString = url.substring(index + 1);
+				Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+				User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+				log.debug("user : {}", user);
+			} else {
+				responseResource(out, url);
+			}
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
 	}
+	
+    private void responseResource(OutputStream out, String url) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
 
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
 		try {
